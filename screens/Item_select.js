0dd4150e7be38bc521b322da51d986abdd18e02f
Gabriel Velazquez Flanegien <gabriel.velazquez4@upr.edu>
2023-04-29 03:08:05 -0400

    Mostly css Itemselect + new imgs
    mejoras en el look de select items y pop up de create items. puse boton de add image todavia esta wip y el teclado cuando se entra precio a numpad. Lo proximo que trabajare sera category colors y Firebase data.

--- screens/Item_select.js
+++ screens/Item_select.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import {StyleSheet, Text, View, Image, Pressable, TextInput, TouchableOpacity, Modal, SafeAreaView, ScrollView, FlatList} from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown' //npm install react-native-select-dropdown
+import { LinearGradient } from "expo-linear-gradient";
 const ItemSelect = ({navigation}) => {//navigation
   
 const data = [
-  { id: '1', name: 'Apple', category: 'Fruit', price: '1.31',image: require('../assets/mydonut.png') },
-  { id: '2',name: 'Banana', category: 'Fruit', price: '1.00', image: require('../assets/mydonut.png') },
-  { id: '3',name: 'Milk', category: 'Dairy', price: '3.50', image: require('../assets/mydonut.png') },
-  { id: '4',name: 'Cheese', category: 'Dairy', price: '4.00', image: require('../assets/mydonut.png') },
+  { id: '1', name: 'Apple', category: 'Fruit', price: '1.31',image: require('../assets/apple.png') },
+  { id: '2',name: 'Banana', category: 'Fruit', price: '1.00', image: require('../assets/banana.png') },
+  { id: '3',name: 'Strawberry', category: 'Fruit', price: '2.00', image: require('../assets/strawberry.png') },
+  { id: '4',name: 'Milk', category: 'Dairy', price: '3.50', image: require('../assets/milk.png') },
+  { id: '5',name: 'Cheese', category: 'Dairy', price: '4.00', image: require('../assets/mydonut.png') },
+  { id: '6',name: 'Donut', category: 'Pastry', price: '2.50', image: require('../assets/mydonut.png') },
 ];
 
 const categoryColors = {
   "fruit": "#F7A0CB",
   "dairy": "#F6EC95",
 };
+
 const [items, setItems] = useState(data);
 const [newItemName, setNewItemName] = useState(data);
 const [selectedCategory, setSelectedCategory] = useState(null);
@@ -33,12 +37,13 @@ const modalhandleCancelCreate = () => {
   //setItemPrice('');
 };
 
+/*console.log(item.name) se remplaza con add to list con correct id */
   const renderItem = (item) => {
     return (
-      <Pressable key={item.id} onPress={() => console.log(item.name)}>
-      <Text style={styles.text}>{item.name}</Text>
+      <TouchableOpacity key={item.id} onPress={() => console.log(item.name)}> 
+      <Text style={styles.itemtext}>{item.name}</Text>
       <Image source={item.image} style={styles.image} />
-    </Pressable>
+    </TouchableOpacity>
     );
   };
 
@@ -46,7 +51,7 @@ const modalhandleCancelCreate = () => {
     const categoryItems = items.filter((item) => item.category === category);
     return (
       <View style={styles.category}>
-        
+      
         <Text style={styles.categoryTitle}>{category}</Text>
         <View style={styles.list}>{categoryItems.map(renderItem)}</View>
       </View>
@@ -65,6 +70,7 @@ const modalhandleCancelCreate = () => {
     setItems([...items, newItem]);
   };
 
+  /*
   const renderAddItem = (category) => {
     return (
       <Pressable key={category} style={styles.addButton}onPress={() => handleAddItem(category)}> 
@@ -72,6 +78,8 @@ const modalhandleCancelCreate = () => {
       </Pressable>
     );
   };
+  */
+
   //^^^ Clone for modal input
   const handleSaveNewItem = (selectedCategory) => { //if  selecteedcategory == category. category: selectedcategory
     setModalVisible(false);
@@ -92,7 +100,7 @@ const modalhandleCancelCreate = () => {
     return categories.map((category) => (
       <View key={category} style={styles.containercat}>
         {renderCategory(category)}
-        {renderAddItem(category)}
+        {/*renderAddItem(category)*/}
       </View>
     ));
   };
@@ -101,6 +109,13 @@ const modalhandleCancelCreate = () => {
   return (
 //Background and Search Bar--------------------------------
     <View style={styles.container}>
+      <LinearGradient style={styles.gradientBackdrop}
+        locations={[0.45, 0.9, 1]}
+        colors={[
+          "#5a71af",
+          "rgba(177, 192, 216, 0.9)",
+          "rgba(195, 212, 233, 0.61)",
+        ]} />
         {/* Header Title */}
         <Text style={styles.title}>Add items to list</Text>
         <Text style={styles.searchtext}>Search Item</Text>
@@ -118,7 +133,7 @@ const modalhandleCancelCreate = () => {
 <View style={styles.buttonContainer}>
         {/* Button 1 */}
         <TouchableOpacity style={styles.buttoncreate} onPress={handleCreateItem}>
-          <Text style={{color:'#000',fontSize: 16,textAlign:'center'}}>Create Item</Text>
+          <Text style={{color:'#000',fontSize: 16,textAlign:'center',}}>Create Item</Text>
         </TouchableOpacity>
 
         {/* Button 2 */}
@@ -137,14 +152,14 @@ const modalhandleCancelCreate = () => {
         <View style={styles.modalContainer}>
           <View style={styles.modal}>
           <Text style={styles.modalTitle}>Create Item</Text>
-            <Text style={styles.modalText}>Item name</Text>
+            <Text style={{fontSize:25,color:'#fff',bottom: 70,right:10}}>Item name</Text>
             <TextInput
-              style={styles.modalInput}
+              style={styles.modalnameInput}
               value={newItemName}
               onChangeText={(text) => setNewItemName(text)}
             />
 {/**/}
-            <SelectDropdown style={styles.dropdownstyle} 
+            <SelectDropdown //style dont work >:( tengo que improvisar
 	data={categories}
 	onSelect={(selectedItem, index) => { 
 		console.log(selectedItem, index)
@@ -161,13 +176,21 @@ const modalhandleCancelCreate = () => {
 		return item
 	}}
 />
-
-              <Text style={styles.modalText}>Item Price (optional)</Text>
+              <Text style={{fontSize:23,color:'#fff', bottom:-10, right:1}}>Item Price (optional)</Text>
             <TextInput
-              style={styles.modalInput}
+              style={styles.modalpriceInput}
               value={newPriceName}
               onChangeText={(text) => setNewPriceName(text)}
+              keyboardType="numeric"
             />
+
+<Text style={{fontSize:23,color:'#fff', bottom:-25, right:50}}>Add image</Text>
+
+<TouchableOpacity
+ style={{backgroundColor: '#636D85',  width: 50,height: 50,borderRadius: 55/2,
+ left:40,bottom:10}} 
+/>
+<Text style={{fontSize:40,color:'#fff', left:40,bottom:60}}>+</Text>
 
            {/*} <TouchableOpacity style={styles.modalButton} onPress={handleSaveItem}>
               <Text style={styles.modalButtonText}>Save</Text>
@@ -203,6 +226,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#5A71AF',
     padding: 20,
     paddingTop:70,
+  },
+  gradientBackdrop: {
+    top: 0,
+    left: 0,
+    borderStyle: "solid",
+    borderColor: "#000",
+    borderWidth: 1,
+    width: 400,
+    backgroundColor: "transparent",
+    position: "absolute",
+    height: 800,
   },
   title: {
     fontSize: 24,
@@ -268,7 +302,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 5,
     color: '#000',
-    backgroundColor: "#F7A0CB",
+    backgroundColor: "#F6EC95",
     width: 70,
     height: 25,
   },
@@ -284,6 +318,13 @@ const styles = StyleSheet.create({
   },
   text: {
     textAlign: 'center',
+  },
+  itemtext:{
+    //fontFamily: FontFamily.interMedium,
+    fontWeight: "500",
+    fontSize: 20,
+    textAlign: "center",
+    color: "#fff",
   },
   addButton: {
     backgroundColor: 'lightgrey',
@@ -304,6 +345,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     padding: 20,
+    marginBottom:10,
   },
   buttoncreate: {
     backgroundColor: '#FEFEFE',
@@ -330,30 +372,51 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   modal: { //modal box
+    /*
     backgroundColor: '#BBC6DA',
-    margin: 80, //size all
+    margin: 90, //size all
    padding: 60, // centralizar interior
     //width:350,
     borderRadius: 30, //curvas
     //flex:.5,//lenght
+    */
+      //top: 223,
+      alignItems: 'center',
+      borderRadius: 30,
+      backgroundColor: '#BBC6DA',
+      width: 340,
+      height: 388,
+     // position: "absolute",
   },
   modalText: {
-   fontSize:20,
-   color:'#fff',
+    fontSize:25,
+    color:'#fff',
+    bottom: 50,
+    right:15,
   },
-  modalInput:{
-    textAlign: "center",
-    alignSelf: "center",
-  borderWidth: 1,
+  modalnameInput:{
   borderColor: '#ccc',
   backgroundColor: '#fff',
   borderRadius: 5,
-  marginBottom: 10,
+ marginBottom: -25,
   width: 200,
-  height: 30,
+  height: 40,
+  bottom: 50,
+  right:0,
+  
+ 
   },
-  dropdownstyle: {
-   },
+
+  modalpriceInput:{
+    borderColor: '#ccc',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    marginBottom: 10,
+    width: 200,
+    height: 40,
+    bottom: -15,
+    right:1,
+    },
   modalTitle:{ //texto arriba del modal
     fontSize: 35,
     fontWeight: 'bold',
@@ -361,9 +424,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     paddingTop: 1 ,
     //position:'absolute',
-    top:'-45%',
+     top:'-15%',
      right:10,
-     marginBottom:-90,
+     marginBottom:80,
   },
   modalbuttonContainer: {
     flexDirection: 'row',
