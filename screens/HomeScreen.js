5ef5a43d7bab09af08bfcdcf1b1b2cddd9672b67
Steven632 <steven.rodriguez18@upr.edu>
2023-04-30 12:06:06 -0400

    botones de create list y create item
    agregué los botones de crear lista y articulos con sus pop-up. hay un error en crear lista que estoy buscando como resolver y tengo que arreglar el diseño del modal de  los articulos

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,43 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Image, SafeAreaView, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, Image, SafeAreaView, ScrollView, TouchableOpacity, Modal, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import ActionButton from 'react-native-action-button';
 
 export default function HomeScreen() {
-  const [list, setList] = useState([]);
   const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible2, setModalVisible2] = useState(false);
+  const [newItemName, setNewItemName] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [lists, setLists] = useState([]);
 
-  function handleAddItem() {
-    const newItem = {
-      id: Math.random().toString(),
-      name: 'New Item',
+  const categories = ['Fruits', 'Vegetables', 'Meat', 'Dairy'];
+
+  const handleSubmit = () => {
+    // Add code to handle the submission of the new item here
+    console.log(newItemName, selectedCategory);
+    setModalVisible(false);
+  };
+
+  const handleSubmit2 = () => {
+    // Add code to handle the submission of the new item here
+    console.log(newItemName, selectedCategory);
+    setModalVisible2(false);
+  };
+  const handleCreateList = () => {
+    const newList = {
+      listName: 'My New List',
+      items: []
     };
-    setList([...list, newItem]);
+    setLists([...lists, newList]);
+    setModalVisible(false);
+    navigation.navigate('Item_select', { list: newList });
   }
+  
+
+  const handleCancel = () => {
+    setModalVisible(false);
+    setModalVisible2(false);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -24,6 +48,15 @@ export default function HomeScreen() {
         />
         <Text style={styles.headerText}>Pocket {'\n'}Shopper</Text>
       </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => setModalVisible(true)}>
+          <Text style={styles.buttonText}>Create List</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => setModalVisible2(true)}>
+          <Text style={styles.buttonText}>Create Item</Text>
+        </TouchableOpacity>
+      </View>
+
       <ScrollView style={styles.scrollView}>
         <View style={styles.divisionShared}>
           <Text style={styles.divisionTitle}>Shared Lists</Text>
@@ -34,102 +67,228 @@ export default function HomeScreen() {
         <View style={styles.divisionArchived}>
           <Text style={styles.divisionTitle}>Archived Lists</Text>
         </View>
-        <View style={styles.content}>
-          {list.length === 0 ? (
-            <Text style={styles.emptyMessage}>Empty</Text>
-          ) : (
-            list.map((item) => (
-              <Text key={item.id} style={styles.item}>
-                {item.name}
-              </Text>
-            ))
-          )}
-        </View>
       </ScrollView>
-      <ActionButton buttonColor="#5469A3" onPress={handleAddItem} />
-    </SafeAreaView>
-  );
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modal}>
+            <Text style={styles.modalTitle}>Create List</Text>
+            {/* Add code for creating a new list here */}
+            <TextInput
+      style={styles.input}
+      placeholder="List Name"
+      onChangeText={(text) => setLists(text)}
+    />
+            <TouchableOpacity style={styles.modalButton} onPress={handleCreateList}>
+              <Text style={styles.buttonText}>Create</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.modalButton} onPress={handleCancel}>
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible2}
+        onRequestClose={() => setModalVisible2(false)}
+      >
+
+<View style={styles.modalContainer}>
+  <View style={styles.modal}>
+    <Text style={styles.modalTitle}>Create Item</Text>
+    <Text style={{ fontSize: 25, color: '#fff' }}>Item Name:</Text>
+    <TextInput
+      style={styles.input}
+      placeholder="Item Name"
+      onChangeText={(text) => setNewItemName(text)}
+    />
+ 
+<View style={styles.categoryContainer}>
+{categories.map((category) => (
+<TouchableOpacity
+key={category}
+style={[
+styles.categoryButton,
+selectedCategory === category && styles.categoryButtonSelected,
+]}
+onPress={() => setSelectedCategory(category)}
+>
+<Text
+style={[
+styles.categoryText,
+selectedCategory === category && styles.categoryTextSelected,
+]}
+>
+{category}
+</Text>
+</TouchableOpacity>
+))}
+</View>
+<TouchableOpacity style={styles.modalButton} onPress={handleSubmit2}>
+<Text style={styles.buttonText}>Create</Text>
+</TouchableOpacity>
+<TouchableOpacity style={styles.modalButton} onPress={handleCancel}>
+<Text style={styles.buttonText}>Cancel</Text>
+</TouchableOpacity>
+</View>
+</View>
+</Modal>
+</SafeAreaView>
+);
 }
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#B1C0D8',
+  flex: 1,
+  backgroundColor: '#FFFFFF',
   },
   header: {
-    backgroundColor: '#5469A3',
-    height: 150,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 50,
-  },
+        backgroundColor: '#5469A3',
+        height: 150,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 50,
+      },
+      logo: {
+            width: 100,
+            height: 80,
+            resizeMode: "contain",
+          },
   headerText: {
-    color: '#000000',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginLeft: 5,
+  fontSize: 24,
+  fontWeight: 'bold',
+  marginLeft: 10,
   },
-  logo: {
-    width: 100,
-    height: 80,
-    resizeMode: "contain",
+  buttonContainer: {
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  paddingVertical: 20,
+  backgroundColor: '#B1C0D8',
+  },
+  button: {
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  borderRadius: 5,
+  backgroundColor: '#87CEFA',
+  },
+  buttonText: {
+  color: '#000',
+  fontWeight: 'bold',
+  fontSize: 16,
   },
   scrollView: {
-    flex: 1,
+  flex: 1,
+  backgroundColor: '#B1C0D8',
+  paddingHorizontal: 10,
   },
   divisionShared: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#868892',
-    backgroundColor: "#FEFEFE",
-    width: '100%',
-    height: 30,
-    marginBottom: 7,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+  fontSize: 24,
+  fontWeight: 'bold',
+  color: '#868892',
+  backgroundColor: "#FEFEFE",
+  width: '100%',
+  height: 40,
+  
+  paddingHorizontal: 10,
+  paddingVertical: 5,
+  borderRadius: 5,
   },
   divisionPersonal: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#868892',
-    backgroundColor: "#FEFEFE",
-    width: '100%',
-    height: 30,
-    marginBottom: 7,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+  fontSize: 24,
+  fontWeight: 'bold',
+  color: '#868892',
+  backgroundColor: "#FEFEFE",
+  width: '100%',
+  height: 40,
+  marginTop: 20,
+  paddingHorizontal: 10,
+  paddingVertical: 5,
+  borderRadius: 5,
   },
   divisionArchived: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#868892',
-    backgroundColor: "#FF784C",
-    width: '100%',
-    height: 30,
-    marginBottom: 7,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+  fontSize: 24,
+  fontWeight: 'bold',
+  color: '#868892',
+  backgroundColor: "#FF784C",
+  width: '100%',
+  height: 40,
+  marginTop: 20,
+  paddingHorizontal: 10,
+  paddingVertical: 5,
+  borderRadius: 5,
   },
   divisionTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  fontSize: 18,
+  fontWeight: 'bold',
   },
   content: {
-    flex: 1,
-justifyContent: 'center',
-alignItems: 'center',
-paddingHorizontal: 20,
-},
-emptyMessage: {
-fontSize: 20,
-fontWeight: 'bold',
-},
-item: {
-fontSize: 16,
-marginBottom: 10,
-},
-scrollView: {
-marginBottom: 70,
-},
-});
+  marginTop: 10,
+  },
+  emptyMessage: {
+  textAlign: 'center',
+  color: '#ccc',
+  marginTop: 40,
+  },
+  item: {
+  fontSize: 16,
+  paddingVertical: 10,
+  borderBottomWidth: 1,
+  borderBottomColor: '#ccc',
+  },
+  modalContainer: {
+  flex: 1,
+  
+  backgroundColor: 'rgba(0,0,0,0.5)',
+  justifyContent: 'center',
+  alignItems: 'center',
+  },
+  modal: {
+  backgroundColor: '#B1C0D8',
+  width: '90%',
+  paddingVertical: 40,
+  paddingHorizontal: 20,
+  borderRadius: 10,
+  alignItems: 'center',
+  },
+  modalTitle: {
+  fontSize: 24,
+  fontWeight: 'bold',
+  marginBottom: 20,
+  },
+  modalTextInput: {
+  width: '100%',
+  borderColor: 'gray',
+  borderWidth: 1,
+  paddingVertical: 10,
+  paddingHorizontal: 15,
+  marginBottom: 20,
+  borderRadius: 5,
+  },
+  modalPicker: {
+  width: '100%',
+  borderColor: 'gray',
+  borderWidth: 1,
+  paddingVertical: 10,
+  paddingHorizontal: 15,
+  marginBottom: 20,
+  borderRadius: 5,
+  },
+  modalButton: {
+  backgroundColor: '#87CEFA',
+ 
+  paddingVertical: 10,
+  paddingHorizontal: 20,
+  borderRadius: 5,
+  marginHorizontal: 5,
+  },
+  });
